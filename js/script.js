ef2d1b2ae9125fd19987b3ff76b84a566b827fdc
Alex Huynh <alexh3925@gmail.com>
2018-10-10 01:13:00 +0000

    added click function and html

--- js/script.js
+++ js/script.js
@@ -8,4 +8,36 @@
 
 
 // DOCUMENT READY FUNCTION BELOW
-
+$(document).ready(function(){
+    $("#shoot").click(function(){
+        var inputValue = $("#input").val();
+        var computerInput = ["rock","paper","scissor"];
+        if(inputValue === "rock" || inputValue === "paper" || inputValue === "scissors"){
+            $("#userChoice").text(inputValue);
+            $("#computerChoice").text();
+            $("#input").val("");
+        }else{
+            $("#input").val("");
+        }
+    });
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+});
