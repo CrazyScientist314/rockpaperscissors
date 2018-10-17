2377c91a377becaa57930f15351668eaba1b03c5
Alex Huynh <alexh3925@gmail.com>
2018-10-17 01:04:31 +0000

    Added random number and conditinals

--- js/script.js
+++ js/script.js
@@ -8,16 +8,41 @@
 
 
 // DOCUMENT READY FUNCTION BELOW
+/*global $*/
 $(document).ready(function(){
+    var userChoice = "";
+    var computerChoice = "";
+    var winner = "";
+    var randomNumber = 0;
     $("#shoot").click(function(){
-        var inputValue = $("#input").val();
-        var computerInput = ["rock","paper","scissor"];
-        if(inputValue === "rock" || inputValue === "paper" || inputValue === "scissors"){
-            $("#userChoice").text(inputValue);
-            $("#computerChoice").text();
+        userChoice = $("#input").val();
+        randomNumber = Math.floor((Math.random() * 3));
+        computerChoice = ["rock","paper","scissors"];
+        var userScore = parseInt($("#userScore").html(),10);
+        var computerScore = parseInt($("#computerScore").html(),10);
+        $("#userChoice").text(userChoice);
+        console.log(userChoice);
+        if(userChoice === "rock" || userChoice === "paper" || userChoice === "scissors"){
+            $("#userChoice").text(userChoice);
+            $("#computerChoice").text(computerChoice[randomNumber]);
+            if((userChoice === "rock" && computerChoice[randomNumber] === "scissors") || (userChoice === "scissors" && computerChoice[randomNumber] === "paper") || (userChoice === "paper" && computerChoice[randomNumber] === "rock")){
+                $("#userScore").text(userScore + 1);
+            }else if((userChoice === "scissors" && computerChoice[randomNumber] === "rock") || (userChoice === "paper" && computerChoice[randomNumber] === "scissors") || (userChoice === "rock" && computerChoice[randomNumber] === "paper")){
+                $("#computerScore").text(computerScore + 1);
+            }else{
+                $("#userScore").text(userScore);
+                $("#computerScore").text(computerScore);
+            }
             $("#input").val("");
         }else{
             $("#input").val("");
+        }
+        if(userScore === 5){
+            alert("You Win");
+        }else if(computerScore === 5){
+            alert("You Lose");
+        }else{
+            
         }
     });
     
@@ -26,6 +51,13 @@ $(document).ready(function(){
     
     
     
+     /*var computerInput = ["rock","paper","scissor"];
+        if(inputValue === "rock" || inputValue === "paper" || inputValue === "scissors"){
+            $("#userChoice").text(inputValue);
+            $("#computerChoice").text();
+            $("#input").val("");
+        }else{
+            $("#input").val("");*/
     
     
     
